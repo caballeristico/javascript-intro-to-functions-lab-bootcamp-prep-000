9c79f5557474157c764aca0392d1174f3e43a72c
Leandro Caballero <caballero_leandro@hotmail.com>
2018-05-14 19:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 
-function sayHiToGrandma(string) {return "I love you, too.",  string.toUpperCase(), string.toLowerCase()}
+function sayHiToGrandma(hello) {return string.toLowerCase()}
 
