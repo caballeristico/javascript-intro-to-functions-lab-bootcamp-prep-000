e154a138f45ed503b818b5705a059ebb559a17b4
Leandro Caballero <caballero_leandro@hotmail.com>
2018-05-14 19:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
-function sayHiToGrandma(string) {return string.toLowerCase()}
-function sayHiToGrandma(string) {return string.toUpperCase()}
+function sayHiToGrandma(string) {if (string.toLowerCase()) {return "I can't hear you!"}}
+
+
 function sayHiToGrandma(string) {return "I love you, too."}
